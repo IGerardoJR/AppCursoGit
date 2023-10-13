faa8a4a6c87b2e88737bf80c69fb5cb1eeac2b13
Isaias Cordova <igerardo0808@gmail.com>
2023-10-12 20:04:57 -0600

    Se cambio el titulo del appBar(Curso Git y GitHub)

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Rama AppBar'),
+      home: const MyHomePage(title: 'Curso Git y Github'),
     );
   }
 }

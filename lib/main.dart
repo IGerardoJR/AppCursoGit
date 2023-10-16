c401fbec850aab6ec39d807734e8a4f96c2beebd
Isaias Cordova <igerardo0808@gmail.com>
2023-10-15 18:26:50 -0600

    Se modifico el appBar[Empieza con github]

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage( title: 'IsaiasApp'),
+      home: const MyHomePage( title: 'Ya empieza con Github'),
 
     );
   }

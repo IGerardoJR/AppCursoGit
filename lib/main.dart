f12bc8dec55f8b85ed79b8b2bfe28b5dea26558d
Isaias G.C <igerardo0808@gmail.com>
2023-10-20 01:03:00 -0600

    Se cambio el color y texto de la barra superior

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,10 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage( title: 'Github vs Gitlab'),
+      home: const MyHomePage( title: 'rama App Bar'),
 
 
     );

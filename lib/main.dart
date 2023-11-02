7462ae23c93c983e96aed58a195b007c96506f38
Isaias Gerardo <100460039+IGerardoJR@users.noreply.github.com>
2023-11-01 23:32:04 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage( title: 'Github vs Gitlab'),
+      home: const MyHomePage( title: 'Github manda'),
 
 
     );

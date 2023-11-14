03226f4477ab738dc66705e4e18ed74764be1822
Isaias Gerardo <100460039+IGerardoJR@users.noreply.github.com>
2023-11-13 22:18:02 -0600

    Espero te este gustando el curso

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter manda'),
+      home: const MyHomePage(title: 'Espero te este gustando el curso'),
     );
   }
 }

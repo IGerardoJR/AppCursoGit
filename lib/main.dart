be0c13dca22d4c732af83e5656034d1b3af394a3
Isaias Gerardo <100460039+IGerardoJR@users.noreply.github.com>
2023-11-13 21:58:09 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Github manda'),
+      home: const MyHomePage(title: 'Lo bueno apenas comienza'),
     );
   }
 }

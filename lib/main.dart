93350a43998ebd39fe9a4e9b3fc96ff834204094
Isaias Cordova P <igerardo0808@gmail.com>
2023-11-17 00:36:04 -0600

    stash prueba

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Me gustan el :',
+              'Me gustan el:',
             ),
             Text(
               '$_counter',
